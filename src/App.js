3f7bdb6620b8f6cee99d65391c12b4a232a996cd
Sam-Varghese <sam.varghese.sica@gmail.com>
2022-05-09 20:45:42 +0530

    Addedreadme

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MenuItem from "./components/homepage/Menu";
 import AboutNMIMS from "./components/aboutNMIMS/aboutNMIMS";
 import AboutTed from "./components/aboutTed/aboutTedPage1";
 import TedxNMIMS from "./components/aboutTEDxNMIMS/tedxNMIMS";
+
 function App() {
     return (
         <div className="headingPage">
