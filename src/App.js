6c9b87215e4edcb788734fcad177f45c616e1951
Sam-Varghese <sam.varghese.sica@gmail.com>
2022-05-11 23:54:40 +0530

    Added some secret console text :smile:

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import TedxNMIMS from "./components/aboutTEDxNMIMS/tedxNMIMS";
 import ContactPage from "./components/contactPage/contactPage";
 import { BackTop, Tooltip } from "antd";
 
-console.log("%cTEDxNMIMS, Indore", "font-size: 3vw; font-weight: 900; color: red")
+console.log(
+    "%cTEDxNMIMS, Indore",
+    "font-size: 3vw; font-weight: 900; color: red; background-color: black; padding: 2vw; margin: 1vw; display: flex; justify-content: center"
+);
 
 console.log(
-    "%cWelcome to TEDxNMIMS official website. \n\nThis website has been made by Mr. Sam Varghese, currently a second year MBATech student at NMIMS, Indore. \nGitHub link for the website: https://github.com/TEDx-NMIMS-Indore/TEDx-NMIMS-Indore-official-website \nProfile Sam Varghese: https://www.linkedin.com/in/sam-varghese-1a7701209/",
-    "color: gold"
+    "%cWelcome to TEDxNMIMS official website. \n\nThis website has been made by Mr. Sam Varghese, currently a second year MBATech student at NMIMS, Indore. \nGitHub link for the website: https://github.com/TEDx-NMIMS-Indore/TEDx-NMIMS-Indore-official-website \nProfile Sam Varghese: https://www.linkedin.com/in/sam-varghese-1a7701209/ \nMail ID of Sam Varghese: sam.varg.2003@gmail.com",
+    "color: yellow; background-color: black; padding: 1vw"
 );
 
 function App() {
