87e48415c9ebb894aa96968fc3e7b360efb98429
Sam-Varghese <sam.varghese.sica@gmail.com>
2022-06-10 19:24:46 +0530

    Sponsor img to webp

--- src/components/sponsors/sponsors2019/sponsors2019.js
+++ src/components/sponsors/sponsors2019/sponsors2019.js
@@ -8,7 +8,7 @@ export default function Sponsors2019() {
             <div className="sponsorCategory">
                 <h2>Title Sponsors</h2>
                 <div className="SponsorsCategoryImages">
-                    <img decoding="async" src="/images/sponsors2019/panasonic.png" />
+                    <img decoding="async" src="/images/sponsors2019/panasonic.webp" />
                     <img decoding="async" src="/images/sponsors2019/lotus.webp" />
                 </div>
             </div>
@@ -22,14 +22,14 @@ export default function Sponsors2019() {
                 <h2>Educational Partners</h2>
                 <div className="SponsorsCategoryImages">
                     <img decoding="async" src="/images/sponsors2019/careerLauncher.webp" />
-                    <img decoding="async" src="/images/sponsors2019/Qikme.png" />
+                    <img decoding="async" src="/images/sponsors2019/Qikme.webp" />
                 </div>
             </div>
             <div className="sponsorCategory">
                 <h2>Digital Marketing Partners</h2>
                 <div className="SponsorsCategoryImages">
                     <img decoding="async" src="/images/sponsors2019/STA.webp" />
-                    <img decoding="async" src="/images/sponsors2019/daya.png" />
+                    <img decoding="async" src="/images/sponsors2019/daya.webp" />
                 </div>
             </div>
         </div>
